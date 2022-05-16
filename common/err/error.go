2aded67f0019af5b9bfb3fe88dec9cea3f02382a
chungtm-tiki <chung.tran1@tiki.vn>
2022-05-16 22:21:21 +0700

    feat: add error code

--- common/err/error.go
+++ common/err/error.go
@@ -1,42 +1,46 @@
 package err
 
 var (
-	NilRouter = error{code: -1, mess: "router is nil"}
+	NilRouter = InternalError{code: -1, mess: "router is nil"}
 
-	NilRequest           = error{code: -2, mess: "request is nil"}
-	TooLongCode          = error{code: -3, mess: "code is too long"}
-	HashPasswordFailed   = error{code: -4, mess: "internal error"}
-	AddMAccountFailed    = error{code: -5, mess: "internal error"}
-	NilPassword          = error{code: -6, mess: "password is empty"}
-	NilMerchantCode      = error{code: -7, mess: "merchant code is empty"}
-	GetMAccountFailed    = error{code: -8, mess: "internal error"}
-	UpdateMAccountFailed = error{code: -9, mess: "internal error"}
-	DeleteMAccountFailed = error{code: -10, mess: "internal error"}
-	AddMMemberFailed     = error{code: -11, mess: "internal error"}
-	EmailExisted         = error{code: -12, mess: "email existed"}
-	CheckExistenceFailed = error{code: -13, mess: "check existence failed"}
-	MerchantCodeExisted  = error{code: -14, mess: "merchant code existed"}
-	EmptyEmail           = error{code: -15, mess: "email is empty"}
-	GetMMemberFailed     = error{code: -16, mess: "internal error"}
-	UpdateMMemberFailed  = error{code: -17, mess: "internal error"}
-	DeleteMMemberFailed  = error{code: -18, mess: "internal error"}
+	NilRequest           = InternalError{code: -2, mess: "request is nil"}
+	TooLongCode          = InternalError{code: -3, mess: "code is too long"}
+	HashPasswordFailed   = InternalError{code: -4, mess: "internal error"}
+	AddMAccountFailed    = InternalError{code: -5, mess: "internal error"}
+	EmptyPassword        = InternalError{code: -6, mess: "password is empty"}
+	EmptyMerchantCode    = InternalError{code: -7, mess: "merchant code is empty"}
+	GetMAccountFailed    = InternalError{code: -8, mess: "internal error"}
+	UpdateMAccountFailed = InternalError{code: -9, mess: "internal error"}
+	DeleteMAccountFailed = InternalError{code: -10, mess: "internal error"}
+	AddMMemberFailed     = InternalError{code: -11, mess: "internal error"}
+	EmailExisted         = InternalError{code: -12, mess: "email existed"}
+	CheckExistenceFailed = InternalError{code: -13, mess: "check existence failed"}
+	MerchantCodeExisted  = InternalError{code: -14, mess: "merchant code existed"}
+	EmptyEmail           = InternalError{code: -15, mess: "email is empty"}
+	GetMMemberFailed     = InternalError{code: -16, mess: "internal error"}
+	UpdateMMemberFailed  = InternalError{code: -17, mess: "internal error"}
+	DeleteMMemberFailed  = InternalError{code: -18, mess: "internal error"}
+
+	EmptyMerchantName = InternalError{code: -19, mess: "merchant name is empty"}
+	EmptyUserName     = InternalError{code: -20, mess: "username is empty"}
+	NotFound          = InternalError{code: -21, mess: "item not found"}
 )
 
-type error struct {
+type InternalError struct {
 	data interface{}
 	code int
 	mess string
 }
 
-func (e error) Data() interface{} {
+func (e InternalError) Data() interface{} {
 	return e.data
 }
 
-func (e error) Code() int {
+func (e InternalError) Code() int {
 	return e.code
 }
 
-func (e error) Error() string {
+func (e InternalError) Error() string {
 	return e.mess
 }
 
